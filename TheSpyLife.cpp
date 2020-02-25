a4b5e74790bef619a5bd1fac67e9330a5e3730f6
Sudeepa_Noble <52537266+SudeepaNoble@users.noreply.github.com>
2020-02-25 17:02:12 +0530

    Add files via upload

---
+++ TheSpyLife.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+   
+	string s;
+
+   	getline(cin,s);
+   
+	for(int i=s.length()-1;i>=0;i--)
+   
+	{
+       
+		if((s[i]>64 && s[i]<91)||(s[i]>96 && s[i]<123)||s[i]==' ')
+
+       cout<<s[i];
+   
+	}
+    
+	return 0;
+}
